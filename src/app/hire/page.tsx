9edc08a19188efba223727689e54f024b1dac1e3
virendra9768 <virendra.shekhawat9768@gmail.com>
2026-06-11 19:58:49 +0530

    feat: simplify HirePage component by redirecting to index.html

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -1,12 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function HirePage() {
-  return (
-    <iframe
-      src="/hire/index.html"
-      style={{
-        width: "100%",
-        height: "100vh",
-        border: "none",
-      }}
-    />
-  );
+  redirect("/hire/index.html");
 }
