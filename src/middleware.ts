969d649b6b9bc573d370351bc16bdec96bdccc9a
virendra9768 <virendra.shekhawat9768@gmail.com>
2026-06-11 19:34:21 +0530

    feat: update middleware to redirect hire subdomain requests to index.html

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const host = request.headers.get("host") || "";
 
-  if (host.includes("hire.virendrashekhawat.com")) {
-    return new NextResponse("HIRE SUBDOMAIN DETECTED");
+  if (host.startsWith("hire.")) {
+    return NextResponse.redirect(
+      new URL("/hire/index.html", request.url)
+    );
   }
 
   return NextResponse.next();
